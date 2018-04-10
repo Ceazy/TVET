f7d2208ae4430a849189b19e0860d62125abbf57
ceazy <crazgyg@gmail.com>
2018-04-10 18:01:52 +1000

    Started programming Player script

--- Assets/~GoneHome/Scripts/Player.cs
+++ Assets/~GoneHome/Scripts/Player.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
-    // Use this for initialization
-    void Start()
-    {
+    public string message = "Hello World!";
+    public Rigidbody rigid;
+    float speed = 5;
+    
         
-    }
+// Use this for initialization
+void Start()
+{
+       
+}
 
-    // Update is called once per frame
-    void Update()
+/*
+[] - Brackets
+{} - Braces
+() - Parenthesis
+
+Hot keys: 
+- Clean Code: CTRL + K + D
+- Fold Code : CTRL + M + O
+- UnFold Codd : CTRL + M + P
+*/
+
+// Update is called once per frame
+void Update()
+{
+    if (Input.GetKeyDown(KeyCode.W))
     {
-
-   }
+            rigid.AddForce(Vector3.forward * speed);
+    }
+}
+}
